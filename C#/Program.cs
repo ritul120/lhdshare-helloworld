6497f76260d81b02f5356a1c508262697e2594ef
Ritul <rituljain10121998@gmail.com>
2021-03-29 17:01:16 +0530

    Initial commit

---
+++ C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}! Nice to see you!");
+        }
+    }
+}
